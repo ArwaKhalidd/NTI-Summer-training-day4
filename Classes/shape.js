e181bfc63ed17789d57017913669dd89f921c04d
Arwa Khalid <arwa.rorokh25@gmail.com>
2025-07-02 13:33:34 +0300

    Update shape.js

--- Classes/shape.js
+++ Classes/shape.js
@@ -1,1 +1,21 @@
+export class Shape {
+    constructor(name) {
+        if (this.constructor == Shape) {
+            throw "invalied obj";
+        }
+        this.name = name;
+    }
+
+    area() {
+        return 0;
+    }
+
+    perimeter() {
+        return 0;
+    }
+
+    toString() {
+        return `Shape: ${this.name}`;
+    }
+}
 
